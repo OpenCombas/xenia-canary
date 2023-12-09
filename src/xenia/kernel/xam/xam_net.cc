fd4e1c3d1360b9e0f3fe0b2674c4e3dda78555a3
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-09 13:51:25 +0000

    [XAM] Fixed XLiveAPI initialisation race condition

--- src/xenia/kernel/xam/xam_net.cc
+++ src/xenia/kernel/xam/xam_net.cc
@@ -359,6 +359,7 @@ dword_result_t NetDll_WSAStartup_entry(dword_t caller, word_t version,
   // NetDll_WSAStartup is called multiple times?
   XELOGI("NetDll_WSAStartup");
 
+  // Must initialise XLiveAPI inside kernel to guarantee timing/race conditions.
   XLiveAPI::Init();
 
   // TODO(benvanik): abstraction layer needed.
@@ -590,6 +591,9 @@ dword_result_t NetDll_XNetGetTitleXnAddr_entry(dword_t caller,
 
   // Halo 3 calls NetDll_XNetGetTitleXnAddr before NetDll_WSAStartup
   
+  // Must initialise XLiveAPI inside kernel to guarantee timing/race conditions.
+  XLiveAPI::Init();
+
   auto status = XnAddrStatus::XNET_GET_XNADDR_STATIC |
                 XnAddrStatus::XNET_GET_XNADDR_GATEWAY |
                 XnAddrStatus::XNET_GET_XNADDR_DNS;
@@ -697,7 +701,9 @@ dword_result_t NetDll_XNetServerToInAddr_entry(dword_t caller, dword_t server_ad
 
   pina->s_addr = htonl(server_addr);
 
-  XELOGI("Server IP: {}", XLiveAPI::ip_to_string(*pina));
+  if (cvars::logging) {
+    XELOGI("Server IP: {}", XLiveAPI::ip_to_string(*pina));
+  }
 
   return X_ERROR_SUCCESS;
 }
