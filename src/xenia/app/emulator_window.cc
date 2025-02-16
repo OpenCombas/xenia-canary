10f92bde7b0c00be3fdc43cfad4f914e771e1316
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-02-16 00:19:06 +0000

    [XAM/UI] Implemented XamShowFriendsUI & XInviteGetAcceptedInfo
    	src/xenia/app/emulator_window.cc

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -949,7 +949,7 @@ bool EmulatorWindow::Initialize() {
     Netplay_menu->AddChild(std::move(Network_mode_menu));
 
     Netplay_menu->AddChild(MenuItem::Create(
-        MenuItem::Type::kString, "&Friends Manager", "",
+        MenuItem::Type::kString, "&Manager", "",
         std::bind(&EmulatorWindow::ToggleFriendsDialog, this)));
 
     Netplay_menu->AddChild(MenuItem::Create(MenuItem::Type::kSeparator));
@@ -1793,7 +1793,7 @@ void EmulatorWindow::ToggleFriendsDialog() {
     disable_hotkeys_ = true;
     emulator_->kernel_state()->BroadcastNotification(kXNotificationSystemUI, 1);
     friends_manager_dialog_ =
-        std::make_unique<FriendsManagerDialog>(imgui_drawer_.get(), this);
+        std::make_unique<ManagerDialog>(imgui_drawer_.get(), this);
     emulator_->kernel_state()->xam_state()->xam_dialogs_shown_++;
   } else {
     disable_hotkeys_ = false;
