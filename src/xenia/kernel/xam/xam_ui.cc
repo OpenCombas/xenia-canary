532ee199ab8e2fe61649d36383e6d45a56ae5475
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-04-03 23:44:33 +0100

    [XAM/UI] Fixed manager crashing when no profiles were signed in

--- src/xenia/kernel/xam/xam_ui.cc
+++ src/xenia/kernel/xam/xam_ui.cc
@@ -2162,6 +2162,10 @@ bool xeDrawAddFriend(ui::ImGuiDrawer* imgui_drawer, UserProfile* profile,
 bool xeDrawFriendsContent(ui::ImGuiDrawer* imgui_drawer, UserProfile* profile,
                           FriendsContentArgs& args,
                           std::vector<FriendPresenceObjectJSON>* presences) {
+  if (!profile || !presences) {
+    return false;
+  }
+
   uint32_t user_index =
       kernel_state()->xam_state()->GetUserIndexAssignedToProfileFromXUID(
           profile->GetLogonXUID());
