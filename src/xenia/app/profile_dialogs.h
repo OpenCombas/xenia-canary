eaf3474648a1cd1cce3cfd265de5d97f1a971f70
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-04-23 11:47:25 +0100

    [XAM/UI] Implemented XamShowCommunitySessionsUI

--- src/xenia/app/profile_dialogs.h
+++ src/xenia/app/profile_dialogs.h
@@ -11,6 +11,7 @@
 #define XENIA_APP_PROFILE_DIALOGS_H_
 
 #include "xenia/kernel/json/friend_presence_object_json.h"
+#include "xenia/kernel/json/session_object_json.h"
 #include "xenia/ui/imgui_dialog.h"
 #include "xenia/ui/imgui_drawer.h"
 #include "xenia/xbox.h"
@@ -37,6 +38,13 @@ struct FriendsContentArgs {
   bool refersh_presence_sync;
   AddFriendArgs add_friend_args = {};
   ImGuiTextFilter filter = {};
+};
+struct SessionsContentArgs {
+  bool first_draw;
+  bool sessions_open;
+  bool filter_own;
+  bool refersh_sessions;
+  bool refersh_sessions_sync;
 };
 }  // namespace xam
 }  // namespace kernel
@@ -104,7 +112,9 @@ class ManagerDialog final : public ui::ImGuiDialog {
   uint64_t selected_xuid_ = 0;
   uint64_t removed_xuid_ = 0;
   xe::kernel::xam::FriendsContentArgs args = {};
+  xe::kernel::xam::SessionsContentArgs sessions_args = {};
   std::vector<xe::kernel::FriendPresenceObjectJSON> presences;
+  std::vector<std::unique_ptr<xe::kernel::SessionObjectJSON>> sessions;
   EmulatorWindow* emulator_window_;
 };
 
