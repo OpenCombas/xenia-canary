b493b9b025ee5385f869484dea97471043df76e4
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2025-10-11 15:38:28 +0100

    [XAM] Fixed XUserCreateAchievementEnumerator from failing due to online XUID usage
    Fixed Ugly Americans: Apocalypsegeddon repeatedly asking to sign into profile.

--- src/xenia/kernel/xam/xam_state.cc
+++ src/xenia/kernel/xam/xam_state.cc
@@ -8,6 +8,7 @@
  */
 
 #include "xenia/kernel/xam/xam_state.h"
+#include "xenia/base/logging.h"
 #include "xenia/emulator.h"
 
 namespace xe {
@@ -81,6 +82,11 @@ UserProfile* XamState::GetUserProfile(uint32_t user_index) const {
 }
 
 UserProfile* XamState::GetUserProfile(uint64_t xuid) const {
+  if (IsOnlineXUID(xuid)) {
+    assert_always();
+    XELOGI("{}: Using online XUID {:016X}", __func__, xuid);
+  }
+
   return profile_manager_->GetProfile(xuid);
 }
 
