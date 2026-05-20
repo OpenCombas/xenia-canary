18a8553386dcb403e26e77cb06f3fb6b18440a89
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2026-05-20 19:12:08 +0100

    [XAM] Added more features to XamQueryLiveHiveA

--- src/xenia/kernel/xam/xam_info.cc
+++ src/xenia/kernel/xam/xam_info.cc
@@ -115,6 +115,12 @@ dword_result_t XamQueryLiveHiveA_entry(
       value = "1";
     } else if (feature_name.value() == "TFAEnabled") {
       value = "1";
+    } else if (feature_name.value() == "CatalogUriRoot") {
+      value = "http://catalog.xboxlive.com";
+    } else if (feature_name.value() == "CatalogCDNUriRoot") {
+      value = "http://catalog-cdn.xboxlive.com";
+    } else if (feature_name.value() == "CatalogCDNUriPort") {
+      value = "80";
     } else {
       assert_always();
       XELOGI("Unknown Feature: {}", feature_name.value());
